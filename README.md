87a634c834265bb22e1d30b50a969fb77c1e2766
hezhenfeng <feng960106@163.com>
2021-05-19 18:01:22 +0800

    img

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # snowpack+vue
+
+
+暂时没有找到vue2 热加载的问题
+
+- [https://www.snowpack.dev/reference/configuration#mode](https://www.snowpack.dev/reference/configuration#mode)
+- [hmr](https://www.snowpack.dev/concepts/hot-module-replacement)
--- src/app.vue
+++ src/app.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="App" id="app">
     <header class="App-header">
-      <!--      <img src="./assets/images/logo.svg" class="App-logo" alt="logo"/>-->
+      <img :src="logo" class="App-logo" alt="logo" />
       <p>
-        1212 121 12 121212 Edit1 1212121 11
-        <code>src/App.vue</code> and save to reload. /{{ type }}/{{ ts }}/{{ yy }}/{{ sss }}
+        Edit <code>src/App.vue</code> and save to reload. /{{ type }}/{{ ts }}/{{ yy }}/{{ sss }}
       </p>
       <a class="App-link" href="https://vuejs.org" target="_blank" rel="noopener noreferrer">{{
         message
@@ -14,9 +13,12 @@
 </template>
 
 <script>
+import logo from "./assets/images/logo.svg";
+
 export default {
   data() {
     return {
+      logo,
       message: "Learn Vue",
       type: import.meta.env.NODE_ENV,
       yy: import.meta.env.SNOWPACK_PUBLIC_ENABLE,
