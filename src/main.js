4fa0b9fccfda822f124dd67fc46ca21add78bbc3
hezhenfeng <feng960106@163.com>
2021-05-19 18:33:58 +0800

    测试  hook

--- src/main.js
+++ src/main.js
@@ -18,8 +18,8 @@
 import Vue from "vue";
 import App from "./app.vue";
 
-let app = new Vue({ render: (h) => h(App) });
-app.$mount(document.getElementById("root"));
+  let app = new Vue({ render: (h) => h(App) });
+  app.$mount(document.getElementById("root"));
 
 if (import.meta.hot) {
   import.meta.hot.accept();
