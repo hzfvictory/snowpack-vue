bfb638a745c7dba86d61c871c002e3214b6f29fe
hezhenfeng <feng960106@163.com>
2021-05-20 14:32:08 +0800

    生产模式 使用 webpack 打包

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    syntax: 'postcss-scss',
-    plugins: [
-      require('@csstools/postcss-sass')({
-        includePaths: ['./node_modules']
-      }),
-      require('autoprefixer')
-    ]
-  }
+  syntax: "postcss-scss",
+  plugins: [
+    require("@csstools/postcss-sass")({
+      includePaths: ["./node_modules"],
+    }),
+    require("autoprefixer")({ overrideBrowserslist: ["> 0.15% in CN"] }),
+  ],
+};
--- snowpack.config.js
+++ snowpack.config.js
@@ -1,3 +1,6 @@
+const CompressionWebpackPlugin = require("compression-webpack-plugin");
+const ProgressBarPlugin = require("progress-bar-webpack-plugin");
+
 module.exports = {
   mode: "development",
   mount: {
@@ -5,11 +8,31 @@ module.exports = {
     src: "/_build_",
   },
   plugins: [
-    "@snowpack/plugin-webpack",
-    // "@snowpack/plugin-vue",
+    [
+      "@snowpack/plugin-webpack",
+      {
+        manifest: true,
+        extendConfig: (config) => {
+          config.plugins.push(
+            new ProgressBarPlugin(),
+            new CompressionWebpackPlugin({
+              filename: "[path].gz[query]", // 压缩后的文件策略
+              algorithm: "gzip", // 压缩方式
+              test: /\.(js|css|html|svg)$/, // 可设置需要压缩的文件类型
+              threshold: 10240, // 大于 10 kb 的文件启用压缩
+              minRatio: 0.8, // 压缩比率大于等于0.8时不进行压缩
+              deleteOriginalAssets: false, // 是否删除压缩前的文件，默认false
+            }),
+          );
+          console.log(1212, config);
+          return config;
+        },
+      },
+    ],
+    // "@snowpack/plugin-vue", // 仅支持 vue3
     "snowpack-vue2-plugin",
     "@snowpack/plugin-sass",
-    "@snowpack/plugin-dotenv",
+    "@snowpack/plugin-dotenv", // 环境变量
   ],
   devOptions: {
     port: 9988,
